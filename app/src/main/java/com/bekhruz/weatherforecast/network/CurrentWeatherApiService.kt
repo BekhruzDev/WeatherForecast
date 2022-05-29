443c84e18fa77df8e288290a4b71bd5a847c2759
BekhruzDev <bekhruzabdullakhujaev@gmail.com>
2022-05-29 17:52:01 +0500

    Added Logging Interceptor

--- app/src/main/java/com/bekhruz/weatherforecast/network/CurrentWeatherApiService.kt
+++ app/src/main/java/com/bekhruz/weatherforecast/network/CurrentWeatherApiService.kt
@@ -4,6 +4,8 @@ import com.bekhruz.weatherforecast.network.currentweather.CurrentForecast
 import com.bekhruz.weatherforecast.utils.Constants.BASE_URL_CURRENT_WEATHER
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -15,6 +17,16 @@ import retrofit2.http.Query
  * https://api.weatherapi.com/v1/forecast.json?key=2024618787f64a0b816101809221805&q=London&days=7&aqi=no&alerts=no
  */
 
+//logging interceptor
+private val interceptor = run {
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.apply {
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+    }
+}
+//OkHttpClient
+private val okHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
 //moshi object
 private val moshi = Moshi
     .Builder()
@@ -25,6 +37,7 @@ private val retrofit = Retrofit
     .Builder()
     .baseUrl(BASE_URL_CURRENT_WEATHER)
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .client(okHttpClient)
     .build()
 
 interface CurrentWeatherApiService {
--- app/src/main/java/com/bekhruz/weatherforecast/network/SixteenDayForecastApiService.kt
+++ app/src/main/java/com/bekhruz/weatherforecast/network/SixteenDayForecastApiService.kt
@@ -4,11 +4,23 @@ import com.bekhruz.weatherforecast.network.sixteendayweather.SixteenDayForecast
 import com.bekhruz.weatherforecast.utils.Constants
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+
+//logging interceptor
+private val interceptor = run {
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.apply {
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+    }
+}
+//OkHttpClient
+private val okHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
 //moshi object
 private val moshi = Moshi
@@ -21,6 +33,7 @@ private val retrofit = Retrofit
     .Builder()
     .baseUrl(Constants.BASE_URL_SIXTEEN_DAY_WEATHER)
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .client(okHttpClient)
     .build()
 
 interface SixteenDayApiService {
