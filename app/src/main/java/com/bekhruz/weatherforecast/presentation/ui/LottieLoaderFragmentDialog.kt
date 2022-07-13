5da5b478bf183809755c04325ab485f5de7ae0b9
Azamat Makhkamov <azamat.makhkamov@midge-medical.com>
2022-07-13 15:05:55 +0500

    Fixed loading

--- app/src/main/java/com/bekhruz/weatherforecast/presentation/ui/LottieLoaderFragmentDialog.kt
+++ app/src/main/java/com/bekhruz/weatherforecast/presentation/ui/LottieLoaderFragmentDialog.kt
@@ -8,36 +8,41 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
+import com.airbnb.lottie.LottieDrawable
 import com.bekhruz.weatherforecast.databinding.FragmentLottieLoaderDialogBinding
 import com.bekhruz.weatherforecast.databinding.FragmentLottieLoaderDialogBinding.*
 
-class LottieLoaderFragmentDialog: DialogFragment() {
+class LottieLoaderFragmentDialog : DialogFragment() {
+
     private lateinit var binding: FragmentLottieLoaderDialogBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = inflate(inflater,container,false)
+        binding = inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (dialog == null){
+        if (dialog == null) {
             return
         }
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-                binding.lottieDotLoader.show()
-                binding.lottieDotLoader.playAnimation()
-        }
+        binding.lottieDotLoader.show()
+        binding.lottieDotLoader.repeatCount = LottieDrawable.INFINITE
+        binding.lottieDotLoader.playAnimation()
 
     }
 
-    companion object{
-        const val TAG ="LottieLoaderFragmentDialog"
+    companion object {
+        const val TAG = "LottieLoaderFragmentDialog"
+        fun getInstance(): LottieLoaderFragmentDialog {
+            return LottieLoaderFragmentDialog()
+        }
     }
 
     private fun View.show() {
